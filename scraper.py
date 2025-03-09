cdba933c7baa36189a678932c3fc1bcd887238ab
Allen Shi <allen_shi2002@hotmail.com>
2025-03-08 21:45:20 -0500

    Added basic webscraping placeholder

--- scraper.py
+++ scraper.py
@@ -1,7 +1,35 @@
-import flask
+from playwright.sync_api import sync_playwright
 
-app = flask.Flask(__name__)
+# Reading from hosts file
+with open('hosts.txt') as file:
+    websites = file.readlines()
 
-@app.route('/home')
-def home():
-    return "Hello World"
+# Cleans up newlines from array
+websites = list(filter(None, map(lambda x:x.strip(), websites)))
+
+
+# Init crawling
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=False, slow_mo=50)
+
+    # Website title array
+    description = []
+
+    # Retrieve from each site
+    for site in websites:
+        page = browser.new_page()
+        page.goto("https://" + site)
+
+        # Waits for main selector to be available
+        main = page.locator('main')
+        main.wait_for()
+
+        # Pushes title to array
+        description.append(page.title())
+
+        # Finished with page
+        page.close()
+
+# Prints items that were retrieved
+print(description)
+
