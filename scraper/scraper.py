2d279c0c754f07c2b675ee7abefd97456e0e5755
Darsh Gandhi <darshg0219@gmail.com>
2025-03-11 18:33:02 -0400

    new stuff

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,13 +3,20 @@ from playwright_stealth import stealth_sync
 import pandas as pd
 import os
 import re
+import time
+import json
+from utils import convert_url
 
 # Variables
 SCREENSHOTS_PATH = "./screenshots"
 
 # Reading from hosts file
 with open('./data/hosts.txt') as file:
-    websites = file.readlines()
+    websites = list(filter(lambda line: not line.lstrip().startswith("*"), map(str.strip, file)))
+
+# Loading selectors from JSON
+with open("./data/selectors.json", "r") as f:
+    selectors = json.load(f)
 
 # Creating dataframe object
 df = pd.DataFrame()
@@ -18,13 +25,17 @@ df = pd.DataFrame()
 websites = list(filter(None, map(lambda x:x.strip(), websites)))
 df['website'] = websites
 
+# Start time
+start_time = time.perf_counter()
+
 # Init crawling
 with sync_playwright() as p:
-    browser = p.firefox.launch(headless=False)
+    browser = p.firefox.launch(headless=True)
 
     # Website title array
     description = []
     html = []
+    details = []
 
     # Retrieve from each site
     for site in websites:
@@ -45,23 +56,41 @@ with sync_playwright() as p:
             page.screenshot(path=screenshot_path)
 
             # Pushes title to array
-            description.append(page.title())
-            html.append(page.content())
+            job_list = page.wait_for_selector(selectors[site]['wait_for'])
+            job_items = job_list.query_selector_all("li")
+            
+            for job in job_items:
+                job_url_element = job.query_selector(selectors[site]['url_class'])
+                job_url = job_url_element.get_attribute("href") if job_url_element else None
+
+                details.append({
+                    "title": job.query_selector(selectors[site]['title_class']).inner_text() if selectors[site]['title_class'] else "N/A",
+                    "type": job.query_selector(f'xpath={selectors[site]['type_xpath']}').inner_text() if selectors[site]['type_class'] else "N/A",
+                    "team": job.query_selector(f'xpath={selectors[site]['team_xpath']}').inner_text() if selectors[site]['team_class'] else "N/A",
+                    "location": job.query_selector(f'xpath={selectors[site]['location_xpath']}').inner_text() if selectors[site]['location_class'] else "N/A",
+                    "url": convert_url(selectors[site]['url_prefix'], selectors[site]['url_regex'], job_url) if job_url else "N/A"
+                })
+            #description.append(job_list.inner_text())
+            #html.append(page.content())
 
             # Finished with page
             page.close()
         except Exception as error:
             print(f"Failed to scrape {site}: {error}")
+            import traceback
+            print("Error occurred:", error)
+            traceback.print_exc()
             description.append(f"{error}")
             html.append(f"{error}")
 
-# Prints items that were retrieved
-#print(description)
-#print(html)
-
 # Update DataFrame
-df['description'] = description
-df['html'] = html
+#df['description'] = description
+#df['html'] = html
+job_df = pd.DataFrame(details)
 
 # Create Excel
-df.to_excel("./output/output.xlsx", sheet_name="HTML")
+#df.to_excel("./output/output.xlsx", sheet_name="HTML")
+job_df.to_excel("./output/output.xlsx", sheet_name="HTML", index=False)
+
+elapsed = time.perf_counter() - start_time
+print(f"Runtime: {elapsed}")
