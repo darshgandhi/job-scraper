8d54a277489696f0ff74bd492a24efc2a526b423
Allen Shi <allen_shi2002@hotmail.com>
2025-03-13 19:12:04 -0400

    Added sync, cleaned up selectors, added pagination navigation

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -1,8 +1,10 @@
-from playwright.sync_api import sync_playwright
-from playwright_stealth import stealth_sync
+import asyncio
+from asyncio import timeout
+from playwright.async_api import async_playwright
+from undetected_playwright import Malenia
 from supabase import create_client, Client
 from dotenv import load_dotenv
-from utils import convert_url
+from urllib.parse import urlparse
 import pandas as pd
 import os
 import re
@@ -25,70 +27,123 @@ os.makedirs("./output", exist_ok=True)
 details = []
 start_time = time.perf_counter()
 
-with sync_playwright() as p:
-    browser = p.firefox.launch(headless=True)
 
-    for site in websites:
-        try:
-            if site not in selectors:
-                print(f"No selectors found for {site}, skipping")
-                continue
-            
-            context = browser.new_context()
-            page = context.new_page()
-            stealth_sync(page)
-            
-            page.goto(site, wait_until="domcontentloaded")
-            page.wait_for_load_state("networkidle")
-            page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
-            
-            page.screenshot(path=os.path.join(SCREENSHOTS_PATH, f"{re.sub(r'[\/:*?"<>|]', '_', site)}.png"))
-            
-            job_list = page.wait_for_selector(selectors[site]['wait_for'], timeout=10000)
-            for job in job_list.query_selector_all("li"):
-                job_details = {"source": site}
-                
-                # URL
-                job_url = None
-                if selectors[site].get('url_class'):
-                    job_url_element = job.query_selector(selectors[site]['url_class'])
-                    if job_url_element:
-                        job_url = job_url_element.get_attribute("href")
-                        url_prefix = selectors[site].get('url_prefix')
-                        url_regex = selectors[site].get('url_regex')
-                        if job_url and url_prefix and url_regex:
-                            job_details["url"] = convert_url(url_prefix, url_regex, job_url)
-                        else:
-                            job_details["url"] = job_url
-                job_details.setdefault("url", "N/A")
-                
-                # Title
-                if selectors[site].get('title_class'):
-                    title_element = job.query_selector(selectors[site]['title_class'])
-                    job_details["title"] = title_element.inner_text() if title_element else "N/A"
-                else:
-                    job_details["title"] = "N/A"
-                
-                # Job Type
-                if selectors[site].get('type_xpath'):
-                    type_element = job.query_selector(f"xpath={selectors[site]['type_xpath']}")
-                    job_details["job_type"] = type_element.inner_text() if type_element else "N/A"
-                else:
-                    job_details["job_type"] = "N/A"
-                
-                # Location
-                if selectors[site].get('location_xpath'):
-                    location_element = job.query_selector(f"xpath={selectors[site]['location_xpath']}")
-                    job_details["location"] = location_element.inner_text() if location_element else "N/A"
-                else:
-                    job_details["location"] = "N/A"
-                
-                details.append(job_details)
+async def scrape_page(job_elements, site, site_details):
+    for job in job_elements:
+        job_details = {"source": site}
 
-            context.close()
-        except Exception as e:
-            print(f"Failed to scrape {site}: {e}")
+        # URL
+        if selectors[site].get('url_xpath'):
+            job_url_element = await job.query_selector(selectors[site]['url_xpath'])
+            if job_url_element:
+                job_href = await job_url_element.get_attribute("href")
+                job_base_url = urlparse(site)
+                job_url = job_base_url.scheme + "://" + job_base_url.netloc + job_href
+                job_details["url"] = job_url
 
-job_df = pd.DataFrame(details)
-job_df.to_excel("./output/output.xlsx", sheet_name="HTML", index=False)
-print(f"Runtime: {time.perf_counter() - start_time:.2f} seconds")
+        # Title
+        if selectors[site].get('title_xpath'):
+            title_element = await job.query_selector(selectors[site]['title_xpath'])
+            job_details["title"] = await title_element.inner_text() if title_element else "N/A"
+
+        # Job Type
+        if selectors[site].get('type_xpath'):
+            type_element = await job.query_selector(selectors[site]['type_xpath'])
+            job_details["job_type"] = await type_element.inner_text() if type_element else "N/A"
+
+        # Location
+        if selectors[site].get('location_xpath'):
+            location_element = await job.query_selector(selectors[site]['location_xpath'])
+            job_details["location"] = await location_element.inner_text() if location_element else "N/A"
+
+        site_details.append(job_details)
+
+
+async def scrape_site(site, browser):
+    try:
+        if site not in selectors:
+            print(f"No selectors found for {site}, skipping")
+            return []
+
+        context = await browser.new_context(locale="en-US")
+        await Malenia.apply_stealth(context)
+        page = await context.new_page()
+
+        await page.goto(site, wait_until="domcontentloaded")
+        await page.wait_for_load_state("networkidle")
+        # await page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+        await page.screenshot(path=os.path.join(SCREENSHOTS_PATH, f"{re.sub(r'[\/:*?"<>|]', '_', site)}.png"))
+
+        site_details = []
+        while True:
+            # Get job list elements
+            job_list = await page.wait_for_selector(selectors[site]['wait_for'], timeout=100000, state='visible')
+            job_elements = await job_list.query_selector_all(selectors[site]['job_elements'])
+
+            # scrape page and then check for next page
+            await scrape_page(job_elements, site, site_details)
+
+            # no pagination xpath, break
+            if not selectors[site].get('pagination_xpath'):
+                break
+
+            pagination = await page.query_selector(selectors[site].get('pagination_xpath'))
+
+            # no next pagination element in document, break
+            if not pagination:
+                break
+
+            # click to next page
+            await pagination.click()
+
+        await context.close()
+        return site_details
+
+    except Exception as e:
+        print(f"Failed to scrape {site}: {e}")
+        return []
+
+
+async def main():
+    async with async_playwright() as p:
+
+        browser = await p.chromium.launch(headless=False)
+        tasks = [scrape_site(site, browser) for site in websites]
+        results = await asyncio.gather(*tasks)
+        global details
+        details = [item for sublist in results for item in sublist]
+        await browser.close()
+
+    job_df = pd.DataFrame(details)
+    job_df.to_excel("./output/output.xlsx", sheet_name="HTML", index=False)
+    print(f"Runtime: {time.perf_counter() - start_time:.2f} seconds")
+
+    if not job_df.empty:
+        existing_urls = []
+        response = supabase.table('jobs').select('url').execute()
+        if hasattr(response, 'data'):
+            existing_urls = [job['url'] for job in response.data]
+            print(f"Found {len(existing_urls)} existing job URLs in database")
+
+        print(job_df)
+        new_jobs_df = job_df[~job_df['url'].isin(existing_urls)]
+        print(f"Found {len(new_jobs_df)} new jobs to insert (out of {len(job_df)} total)")
+
+        if not new_jobs_df.empty:
+            new_jobs_df['company_id'] = new_jobs_df.get('company_id', None)
+            new_jobs_df['description'] = new_jobs_df.get('description', "N/A")
+
+            job_records = new_jobs_df.to_dict(orient='records')
+            if job_records:
+                response = supabase.table('jobs').insert(job_records).execute()
+                print(f"Successfully inserted {len(job_records)} new jobs!")
+            else:
+                print("No new jobs to insert")
+        else:
+            print("All jobs are already in the database")
+    else:
+        print("No jobs found to insert into database.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
